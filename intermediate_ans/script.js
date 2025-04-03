90ba2a14c14d877c330a35df325d8e3dac5865f5
sasori-256 <sasori.0274@gmail.com>
2025-04-04 08:42:49 +0900

    add: add answer directories and make problems

--- intermediate_ans/script.js
+++ intermediate_ans/script.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   // DOMの読み込みが完了したら実行
-  // const modal = document.getElementById("modal");
-  // const modalImg = document.getElementById("modal-img");
-  // const modalName = document.getElementById("modal-name");
-  // const modalRole = document.getElementById("modal-role");
-  // const modalDescription = document.getElementById("modal-description");
-  // const closeBtn = document.querySelector(".close");
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const modalName = document.getElementById("modal-name");
+  const modalRole = document.getElementById("modal-role");
+  const modalDescription = document.getElementById("modal-description");
+  const closeBtn = document.querySelector(".close");
 
   // プロフィール画像をクリックしたらモーダルを表示
   document.querySelectorAll(".profile-card").forEach((card) => {
     // .profile-cardクラスの設定された各要素を取得
-    // card.addEventListener("click", function () {
+    card.addEventListener("click", function () {
       // ブラウザでクリック時
-      // modalImg.src = this.getAttribute("data-img");
-      // modalName.textContent = this.getAttribute("data-name");
-      // modalRole.textContent = this.getAttribute("data-role");
-      // modalDescription.textContent = this.getAttribute("data-description");
+      modalImg.src = this.getAttribute("data-img");
+      modalName.textContent = this.getAttribute("data-name");
+      modalRole.textContent = this.getAttribute("data-role");
+      modalDescription.textContent = this.getAttribute("data-description");
 
       // モーダルを表示
-      // modal.style.display = "block";
+      modal.style.display = "block";
     });
   });
 
